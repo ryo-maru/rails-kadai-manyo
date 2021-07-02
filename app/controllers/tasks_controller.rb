0da5e7994211866ee124da1eeb8b72175409b747
ryo maruyama <t.t.uncle0803@gmail.com>
2021-07-02 22:40:45 +0900

    編集機能追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+   before_action :set_task, only: [:show, :edit, :update]
+
   def index
     @tasks = Task.all
   end
@@ -8,17 +10,38 @@ class TasksController < ApplicationController
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def create
-    Task.create(task_params)
-    redirect_to new_task_path
+    @task = Task.new(task_params)
+    if @task.save
+      # 一覧画面へ遷移して"ブログを作成しました！"とメッセージを表示します。
+      redirect_to tasks_path, notice: "ブログを作成しました！"
+    else
+      # 入力フォームを再描画します。
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+
+  def update
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: "ブログを編集しました！"
+    else
+      render :edit
+    end
   end
 
   private
     def task_params
       params.require(:task).permit(:title, :content)
+    end
+
+    def set_task
+      @task = Task.find(params[:id])
     end
 
 end
